dd4702bec23ffc2eca4603d8b8e6cec72de6eea7
Jun Hernandez <jhern254@ucr.edu>
2021-10-07 16:32:38 -0700

    Added box to pfr.

--- R/mod_pfr_screen.R
+++ R/mod_pfr_screen.R
@@ -13,11 +13,22 @@ mod_pfr_screen_ui <- function(id){
   tagList(
     fluidRow(
         h1("Project Financial Report"),
-        column(12, rHandsontableOutput(ns("pfr_table_1")))
+    ),
+    box(
+        title = "PFR",
+        width = 11,         # 12 gives 2nd scroll bar
+        height = "500px",
+        solidHeader = TRUE,
+#        background = "gray-dark",  # TODO: use after changing to DTedit tables
+        closable = FALSE,
+        maximizable = TRUE,
+        rHandsontableOutput(ns("pfr_table_1"))
     )
+
   )
 }
     
+
 #' pfr_screen Server Functions
 #'
 #' @noRd 
@@ -26,29 +37,30 @@ mod_pfr_screen_server <- function(id){
     ns <- session$ns
  
 # TEMP BAD CODE - for demo purposes
-pfr_df <- data.frame("Project ID" = 1:20,
-                     "Fund Code" = 1:20,
-                     "Department" = letters[1:20],
-                     "PI" = letters[1:20],
-                     "Sponsor" = letters[1:20],
-                     "Title" = letters[1:20],
-                     "Reference Award Number" = 1:20,
-                     "Start Date" = seq(from = Sys.Date(), by = "days", length.out = 20),
-                     "End Date" = seq(from = Sys.Date(), by = "days", length.out = 20),
-                     "Status" = letters[1:20],
-                     "F and A Rate" = 1:20,
-                     "Total Revenue" = 1:20,
-                     "Budget" = 1:20,
-                     "Total Expenses" = 1:20,
-                     "Balance" = 1:20,
-                     "Encumbrance" = 1:20,
-                     "Available Balance" = 1:20,
-                     "Costsharing" = 1:20,
-                     "Balance after Cost Sharing" = 1:20,
+pfr_df <- data.frame("Project ID" = 1:26,
+                     "Fund Code" = 1:26,
+                     "Department" = letters[1:26],
+                     "PI" = letters[1:26],
+                     "Sponsor" = letters[1:26],
+                     "Title" = letters[1:26],
+                     "Reference Award Number" = 1:26,
+                     "Start Date" = seq(from = Sys.Date(), by = "days", length.out = 26),
+                     "End Date" = seq(from = Sys.Date(), by = "days", length.out = 26),
+                     "Status" = letters[1:26],
+                     "F and A Rate" = 1:26,
+                     "Total Revenue" = 1:26,
+                     "Budget" = 1:26,
+                     "Total Expenses" = 1:26,
+                     "Balance" = 1:26,
+                     "Encumbrance" = 1:26,
+                     "Available Balance" = 1:26,
+                     "Costsharing" = 1:26,
+                     "Balance after Cost Sharing" = 1:26,
                      check.names = FALSE
         )
 
-pfr_table <- rhandsontable(pfr_df, rowHeaders = NULL)
+# set height
+pfr_table <- rhandsontable(pfr_df, rowHeaders = NULL, height = 450)
 
 
     output$pfr_table_1 <- renderRHandsontable({
